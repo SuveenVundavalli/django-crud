b6f23fe93ba6837260f5b79797dae87fe67ffae3
Suveen Kumar Vundavalli <suveenkumar.vundavalli@gmail.com>
2018-01-09 19:25:25 +0530

    added bootstrap

--- myenv/src/restaurants/views.py
+++ myenv/src/restaurants/views.py
@@ -4,18 +4,26 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home(request):
-    msg = "Hello World!"
-    html_ = f'''
-    <html>
-        <head>
-            <title>Hello</title>
-        </head>
-        <body>
-            <marquee>{msg}</marquee>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html_)
-    # return render(request, "home.html", {}) #response
+# def home(request):
+#     msg = "Hello World!"
+#     html_ = f'''
+#     <html>
+#         <head>
+#             <title>Hello</title>
+#         </head>
+#         <body>
+#             <marquee>{msg}</marquee>
+#         </body>
+#     </html>
+#     '''
+#     return HttpResponse(html_)
+#     # return render(request, "home.html", {}) #response
 
+def home(request):
+    return render(request, "home.html", {"msg": "Hello World!", "var1": "Suveen"})  # response
+
+def about(request):
+    return render(request, "about.html", {"msg": "Hello World!", "var1": "Suveen"})  # response
+
+def contact(request):
+    return render(request, "contact.html", {"msg": "Hello World!", "var1": "Suveen"})  # response
